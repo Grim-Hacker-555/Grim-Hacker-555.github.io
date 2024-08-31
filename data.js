49b409726a2007d2cc32ab98c4b8b1a186f0abdb
Grim Hacker 555 <51239639+Grim-Hacker-555@users.noreply.github.com>
2024-08-31 14:50:55 +0700

    data.js

---
+++ data.js
@@ -0,0 +1,15 @@
+// data.js
+const products = [
+    {
+        id: 1,
+        name: 'สินค้า A1',
+        description: 'รายละเอียดสินค้า A1',
+        price: '100 บาท'
+    },
+    {
+        id: 2,
+        name: 'สินค้า A2',
+        description: 'รายละเอียดสินค้า A2',
+        price: '200 บาท'
+    }
+];
